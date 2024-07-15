873f081cb2e0b3ec6cf67c9300a84db91213e167
denis-dorofeev61224 <denis_dorofeev_1977@mail.ru>
2024-07-16 02:52:03 +0300

    Homework done.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world of Strings!");
+
+
+        //task1
+        String firstName = "Степан";
+        String middleName = "Алексеевич";
+        String lastName = "Дегтярев";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф.И.О.сотрудника-" + fullName);
+        System.out.println("***end of the task 1*** ");
+
+        //task 2
+        String upperFullName = fullName.toUpperCase();
+        System.out.println("Данные Ф.И.О.сотрудника для заполнения отчёта-" + upperFullName);
+        System.out.println("***end of the task 2*** ");
+
+        //task3
+        fullName = "Ёжиков Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        fullName = fullName.replace("Ё", "Е");
+        System.out.println("Данные Ф.И.О.сотрудника-" + fullName);
+        System.out.println("***end of the task 3*** ");
+        System.out.println("***end homework*** ");
+
+
     }
 }
